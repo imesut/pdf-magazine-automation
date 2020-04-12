73ac8aeb40255076ee7a4fc0adf73fc73e32d69b
Mesut Yılmaz <mesut.yilmaz@yga.org.tr>
2020-04-12 16:55:08 +0300

    Refactored layout analysis as seperate function

--- pdf.py
+++ pdf.py
@@ -36,8 +36,8 @@ pagenos=set()
 output_file_count = 1
 prev_biggest_item_size = 0
 
-def eliminate_log(pk, desc, *args):
-    log_file.write(pk + " ELIMINATED: " + desc + str(*args) + "\n")
+def eliminate_log(pN, desc, *args):
+    log_file.write("p: " + pN + " ELIMINATED: " + desc + str(*args) + "\n")
 
 def create_file():
     global output_file_count
@@ -58,7 +58,6 @@ def get_text_size(horizontal_text_line):
     # size returns an array like [7, 1, 1, 1, 1] if just first letter is high
     # We'll return the last item
     return size[-1]
-
 
 output_text_file = create_file()
 
@@ -91,58 +90,57 @@ def calculate_titles_and_insert(text_boxes):
         first_time = False
     return biggest_items
 
+def process_layout(layout):
+    text_boxes = []
+    for element in layout:
+        if isinstance(element, LTTextBoxHorizontal):
+            text_box = element
+            # Calculates text's column number
+            # (Sometimes two successing texts x value differ a little. eliminating differences by assigning col values)
+            xP = round(text_box.x0 / col_width_divider)
+            yP = height-round(text_box.y1)
+            xS = round(text_box.x1 / col_width_divider) 
+            yS = height-round(text_box.y0)
+                
+            # Plain
+            text = text_box.get_text()
+            # Cleaned
+            text = text.replace("-\n", "").replace("\n", "")
+
+            if (yP < footer_threshold):
+                # RULE: Eliminate single word text which is close to the threshold
+                if((yP < header_threshold * 2 or yP > footer_threshold_2) and len(text.split(" ")) < 2):
+                    eliminate_log(pk, "Not in threshold but single word", text)
+                elif(len(text) < 2):
+                    eliminate_log(pk, "Text Length is too small", text)
+                else:
+                    for line in text_box:
+                        if isinstance(line, LTTextLineHorizontal): # Be sure line is an exact line =) 
+                            size = get_text_size(line)
+                            text_boxes.append((xP, yP, xS, yS, size, text, len(text)))
+                            break # analysis of first line is enough
+            else:
+                eliminate_log(pk, "Header / Footer text:", xP, yP, text)
+    return text_boxes
+
 for pN, page in enumerate(PDFPage.get_pages(fp, pagenos, password=password, caching=caching, check_extractable=True)):
     # Cover page ignored.
     if pN > 0 and pN == 31: # pN >= 29 and 
         interpreter.process_page(page)
-        layout = device.get_result()
-        #print(layout)
-        text_boxes = []
-        title = ""
-
-        width = round(page.mediabox[2])
-        height = round(page.mediabox[3])
         
+        # Layout Initializations
+        layout = device.get_result()
+        ## Each page may have different width and height
+        width, height = round( page.mediabox[2] ), round( page.mediabox[3] )
+        col_width_divider = width / col_count
         half_line = height / 2
         header_threshold = 0 #round(height * threshold_rate)
         footer_threshold = round(height * (1 - threshold_rate))
         footer_threshold_2 = round(height * (1 - threshold_rate * 2))
-        
-        # pk -> page key
-        pk = "p" + str(pN)
-        col_width_divider = width / col_count
 
-        print(pk, "PAGE INFO: ", "width", width, "height", height, "header_threshold", header_threshold, "footer_threshold", footer_threshold)
+        print(pN, "PAGE INFO: ", "width", width, "height", height, "header_threshold", header_threshold, "footer_threshold", footer_threshold)
 
-        for element in layout:
-            if isinstance(element, LTTextBoxHorizontal):
-                text_box = element
-                # Calculates text's column number
-                # (Sometimes two successing texts x value differ a little. eliminating differences by assigning col values)
-                xP = round(text_box.x0 / col_width_divider)
-                yP = height-round(text_box.y1)
-                xS = round(text_box.x1 / col_width_divider) 
-                yS = height-round(text_box.y0)
-                
-                # Plain
-                text = text_box.get_text()
-                # Cleaned
-                text = text.replace("-\n", "").replace("\n", "")
-
-                if (yP < footer_threshold):
-                    # RULE: Eliminate single word text which is close to the threshold
-                    if((yP < header_threshold * 2 or yP > footer_threshold_2) and len(text.split(" ")) < 2):
-                        eliminate_log(pk, "Not in threshold but single word", text)
-                    elif(len(text) < 2):
-                        eliminate_log(pk, "Text Length is too small", text)
-                    else:
-                        for line in text_box:
-                            if isinstance(line, LTTextLineHorizontal): # Be sure line is an exact line =) 
-                                size = get_text_size(line)
-                                text_boxes.append((xP, yP, xS, yS, size, text, len(text)))
-                                break # analysis of first line is enough
-                else:
-                    eliminate_log(pk, "Header / Footer text:", xP, yP, text)
+        text_boxes = process_layout(layout)
 
         if (len(text_boxes) > 0):
             text_boxes.sort(key = operator.itemgetter(0, 1))
@@ -153,24 +151,21 @@ for pN, page in enumerate(PDFPage.get_pages(fp, pagenos, password=password, cach
             
             # TODO seperate this function
             titles = calculate_titles_and_insert(text_boxes)
-            print(titles)
             for item in titles:
                 text_boxes.remove(item)
 
             content = ""
             ymax, bottom_item = 0, 0
 
+            # RULE: remove footer fext like page number and magazine name
             for i in text_boxes:
                 if i[1] > ymax:
                     ymax = i[1]
                     bottom_item = i
-
-            # RULE: remove footer fext like page number and magazine name
             text_boxes.remove(bottom_item)
 
-            # TODO: Call seperator
+            # TODO: Call seperator            
             # TODO: Organize vals order
-
             for i in text_boxes:
                 print(i[0], i[1], i[2], i[3], i[4], i[5][:20], len(i[5]))
                 size = i[4]
@@ -183,7 +178,7 @@ for pN, page in enumerate(PDFPage.get_pages(fp, pagenos, password=password, cach
                     content += text + "\n"
 
                 else:
-                    eliminate_log(pk, "Smallest text in the page", text)
+                    eliminate_log(pN, "Smallest text in the page", text)
                 
             output_text_file.write(content)
             print("\n")
