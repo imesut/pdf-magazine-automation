af4b37cff090239916d57ec744618d49cf9868a5
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2020-04-12 20:41:03 +0300

    Added seperator cases for the pages has two section article positioned vertically

--- pdf.py
+++ pdf.py
@@ -1,8 +1,8 @@
 # TODO: remove images with ghostscript
 # TODO stringio may be used
-# TODO add kays to arrays for performance
+# TODO add keys to arrays for performance
 # TODO: What can be parameters
-# 1. col_count - 12 worked for Atlas - 10 worked for Ist. Life
+# 1. col_count - 12 worked for Atlas | 10 worked for Ist. Life
 
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter, PDFPageAggregator
@@ -11,6 +11,7 @@ from pdfminer.pdfpage import PDFPage
 from io import StringIO
 from matplotlib import pyplot as plt
 import operator
+from seperator import get_seperator_line
 
 pdf_file_name = "atlas_text"
 article = ""
@@ -29,15 +30,15 @@ laparams = LAParams(detect_vertical=True)
 device = PDFPageAggregator(resourcemanager, laparams=laparams)
 fp = open(pdf_file_name + ".pdf", 'rb')
 interpreter = PDFPageInterpreter(resourcemanager, device)
-password = ""
 caching = True
 pagenos=set()
 
 output_file_count = 1
 prev_biggest_item_size = 0
+ord_x_pri, ord_y_pri, ord_x_sec, ord_y_sec, ord_size, ord_text = 0, 1, 2, 3, 4, 5
 
 def eliminate_log(pN, desc, *args):
-    log_file.write("p: " + pN + " ELIMINATED: " + desc + str(*args) + "\n")
+    log_file.write("p: " + str(pN) + " ELIMINATED: " + desc + str(*args) + "\n")
 
 def create_file():
     global output_file_count
@@ -68,7 +69,9 @@ def calculate_titles_and_insert(text_boxes):
 
     global prev_biggest_item_size, output_text_file, first_time
 
-    biggest_item = max(text_boxes, key=operator.itemgetter(4))
+    #print("text_boxes", text_boxes)
+    biggest_item = max(text_boxes, key=operator.itemgetter(ord_size))
+    #print("biggest_item", biggest_item)
     biggest_item_size = biggest_item[4]
     biggest_items = []
     if prev_biggest_item_size == 0:
@@ -78,11 +81,14 @@ def calculate_titles_and_insert(text_boxes):
     # if biggest_item_size >= prev_biggest_item_size:
     # There is a threshold
     if abs(biggest_item_size - prev_biggest_item_size) <= 2:
-        biggest_items = list(filter(lambda x: x[4] == biggest_item_size, text_boxes))
-        biggest_items_text = map(lambda x: x[5], biggest_items)
+        biggest_items = list(filter(lambda x: x[ord_size] == biggest_item_size, text_boxes))
+        biggest_items_text = map(lambda x: x[ord_text], biggest_items)
+        # print("biggest_items", biggest_items)
         title = " ".join(biggest_items_text)
+        title = title.capitalize()
+        #print("title", title)
         if not first_time:
-            print("closing file", biggest_item_size, prev_biggest_item_size)
+            #print("closing file", biggest_item_size, prev_biggest_item_size)
             output_text_file.close()
             output_text_file = create_file()
         output_text_file.write("# " + title + "\n")
@@ -90,7 +96,7 @@ def calculate_titles_and_insert(text_boxes):
         first_time = False
     return biggest_items
 
-def process_layout(layout):
+def process_layout(layout, pN):
     text_boxes = []
     for element in layout:
         if isinstance(element, LTTextBoxHorizontal):
@@ -110,78 +116,98 @@ def process_layout(layout):
             if (yP < footer_threshold):
                 # RULE: Eliminate single word text which is close to the threshold
                 if((yP < header_threshold * 2 or yP > footer_threshold_2) and len(text.split(" ")) < 2):
-                    eliminate_log(pk, "Not in threshold but single word", text)
+                    eliminate_log(pN, "Not in threshold but single word", text)
                 elif(len(text) < 2):
-                    eliminate_log(pk, "Text Length is too small", text)
+                    eliminate_log(pN, "Text Length is too small", text)
                 else:
                     for line in text_box:
                         if isinstance(line, LTTextLineHorizontal): # Be sure line is an exact line =) 
                             size = get_text_size(line)
-                            text_boxes.append((xP, yP, xS, yS, size, text, len(text)))
+                            text_boxes.append((xP, yP, xS, yS, size, text))
                             break # analysis of first line is enough
             else:
-                eliminate_log(pk, "Header / Footer text:", xP, yP, text)
+                eliminate_log(pN, "Header / Footer text:", xP, yP, text)
     return text_boxes
 
-for pN, page in enumerate(PDFPage.get_pages(fp, pagenos, password=password, caching=caching, check_extractable=True)):
+def generate_content(text_boxes):
+    content = ""
+    for i in text_boxes:
+        #print( i[ord_x_pri], i[ord_y_pri], i[ord_x_sec], i[ord_y_sec], i[ord_size], i[ord_text][:20] )
+        size = i[ord_size]
+        text = i[ord_text]
+        add_new_line = True
+        #print(text)
+            
+        # RULE: Remove smallest text - generally image descriptions
+        # Smallest size, I'm sceptical about this.If there is no footer text smallest size will be main text's size
+        smallest_size = min(text_boxes, key=operator.itemgetter(ord_size))[ord_size]
+        if size != smallest_size:
+            # RULE: Merge two lines if second one is beginning with lowercase
+            if text[0] == text[0].lower(): # sentence begins with lower letter
+                # print("found lowercase beginning")
+                # print("content" + content)
+                content = content[:-2] + " " # Remove new line chars (\n) from previous line
+            elif text[-1] not in [".", "!", "?"]:
+                add_new_line = False
+            content += text
+            if add_new_line : content += "\n"
+            
+        else:
+            eliminate_log(pN, "Smallest text in the page", text)
+            
+    output_text_file.write(content)
+
+def process_article_block(block):
+    block.sort(key = operator.itemgetter(ord_x_pri, ord_y_pri))
+    titles = calculate_titles_and_insert(block)
+    for item in titles:
+        block.remove(item)
+    generate_content(block)
+
+for pN, page in enumerate(PDFPage.get_pages(fp, pagenos, caching=caching, check_extractable=True)):
     # Cover page ignored.
-    if pN > 0 and pN == 31: # pN >= 29 and 
+    if pN > 0 and pN == 27:
         interpreter.process_page(page)
-        
-        # Layout Initializations
+        # LAYOUT INITIALIZATION - Each page may have different width and height
         layout = device.get_result()
-        ## Each page may have different width and height
         width, height = round( page.mediabox[2] ), round( page.mediabox[3] )
         col_width_divider = width / col_count
         half_line = height / 2
         header_threshold = 0 #round(height * threshold_rate)
         footer_threshold = round(height * (1 - threshold_rate))
         footer_threshold_2 = round(height * (1 - threshold_rate * 2))
-
+        
         print(pN, "PAGE INFO: ", "width", width, "height", height, "header_threshold", header_threshold, "footer_threshold", footer_threshold)
 
-        text_boxes = process_layout(layout)
+        text_boxes = process_layout(layout, pN)
 
-        if (len(text_boxes) > 0):
-            text_boxes.sort(key = operator.itemgetter(0, 1))
-            
-            # TODO These calculations can be more efficient
-            # Smallest size, I'm sceptical about this.If there is no footer text smallest size will be main text's size
-            smallest_size = min(text_boxes, key=operator.itemgetter(4))[4]
-            
-            # TODO seperate this function
-            titles = calculate_titles_and_insert(text_boxes)
-            for item in titles:
-                text_boxes.remove(item)
-
-            content = ""
-            ymax, bottom_item = 0, 0
+        if (len(text_boxes) > 0):            
+            text_boxes.sort(key = operator.itemgetter(ord_x_pri, ord_y_pri))
 
             # RULE: remove footer fext like page number and magazine name
-            for i in text_boxes:
-                if i[1] > ymax:
-                    ymax = i[1]
-                    bottom_item = i
+            bottom_item = max(text_boxes, key=operator.itemgetter(ord_x_pri))
             text_boxes.remove(bottom_item)
 
-            # TODO: Call seperator            
-            # TODO: Organize vals order
-            for i in text_boxes:
-                print(i[0], i[1], i[2], i[3], i[4], i[5][:20], len(i[5]))
-                size = i[4]
-                text = i[-2]
-                # RULE: Remove smallest text - generally image descriptions
-                if size != smallest_size:
-                    # RULE: Merge two lines if second one is beginning with lowercase
-                    if text[0] == text[0].lower(): # sentence begins with lower letter
-                        content = content[:-2] + " " # Remove new line chars
-                    content += text + "\n"
+            # Dividing Page in two part with seperator
+            seperator_line, space = get_seperator_line( list(map(lambda item: [item[ord_y_pri], item[ord_y_sec]], text_boxes)) )
+            
+            print("space", space)
+            print("seperator line: ", seperator_line)
 
-                else:
-                    eliminate_log(pN, "Smallest text in the page", text)
+            if space > 20:
+                above_the_seperator_items = list(filter(lambda text_box: text_box[ord_y_sec] < seperator_line, text_boxes))
+                for item in above_the_seperator_items: text_boxes.remove(item)
+                below_the_seperator_items = text_boxes
+                del text_boxes
+
+                #print(above_the_seperator_items)
+                #print(below_the_seperator_items)
                 
-            output_text_file.write(content)
-            print("\n")
+                process_article_block(above_the_seperator_items)
+                process_article_block(below_the_seperator_items)
+
+            else:
+                process_article_block(text_boxes)
 
 fp.close()
 device.close()
