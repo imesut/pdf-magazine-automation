b2c46b32a361d0214cfce2c3b9ebb3d3e6fe8991
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2020-04-17 19:57:48 +0300

    Title and footer text approaches reconsidered
    eliminated tabular info, changed title extraction logic, added some rules for minimum text length, disabled smallest size removal

--- pdf.py
+++ pdf.py
@@ -1,3 +1,5 @@
+
+
 # TODO: remove images with ghostscript
 # TODO stringio may be used
 # TODO add keys to arrays for performance
@@ -13,14 +15,15 @@ from matplotlib import pyplot as plt
 import operator
 from seperator import get_seperator_line
 
-pdf_file_name = "atlas_text"
+pdf_file_name = "pozitif"
 article = ""
-vals = []
+plt_vals = []
+# x_axis = []
 log_file = open(pdf_file_name + ".log", "w")
 
 # TODO: Change this value for a common approach
 threshold_rate = 0.05
-col_count = 12
+col_count = 10
 
 resourcemanager = PDFResourceManager()
 retstr = StringIO()
@@ -38,11 +41,11 @@ prev_biggest_item_size = 0
 ord_x_pri, ord_y_pri, ord_x_sec, ord_y_sec, ord_size, ord_text = 0, 1, 2, 3, 4, 5
 
 def eliminate_log(pN, desc, *args):
-    log_file.write("p: " + str(pN) + " ELIMINATED: " + desc + str(*args) + "\n")
+    log_file.write("p: " + str(pN) + " ELIMINATED: " + desc + str(args) + "\n")
 
 def create_file():
     global output_file_count
-    otf = open(pdf_file_name + "_" + str(output_file_count) + ".md", "w")
+    otf = open(pdf_file_name + "_" + str(output_file_count) + ".txt", "w")
     output_file_count += 1
     return otf
 
@@ -80,8 +83,12 @@ def calculate_titles_and_insert(text_boxes):
     
     # if biggest_item_size >= prev_biggest_item_size:
     # There is a threshold
+    print("Title calculation: ", prev_biggest_item_size, biggest_item_size)
+    # Normalize too big titles
+    if biggest_item_size > prev_biggest_item_size:
+        biggest_item_size = prev_biggest_item_size
     if abs(biggest_item_size - prev_biggest_item_size) <= 2:
-        biggest_items = list(filter(lambda x: x[ord_size] == biggest_item_size, text_boxes))
+        biggest_items = list(filter(lambda x: x[ord_size] >= biggest_item_size, text_boxes))
         biggest_items_text = map(lambda x: x[ord_text], biggest_items)
         # print("biggest_items", biggest_items)
         title = " ".join(biggest_items_text)
@@ -110,23 +117,30 @@ def process_layout(layout, pN):
                 
             # Plain
             text = text_box.get_text()
-            # Cleaned
-            text = text.replace("-\n", "").replace("\n", "")
 
-            if (yP < footer_threshold):
-                # RULE: Eliminate single word text which is close to the threshold
-                if((yP < header_threshold * 2 or yP > footer_threshold_2) and len(text.split(" ")) < 2):
-                    eliminate_log(pN, "Not in threshold but single word", text)
-                elif(len(text) < 2):
-                    eliminate_log(pN, "Text Length is too small", text)
+            # Eliminate tablular data
+            if text.find("\t") == -1:
+                # Cleaned
+                text = text.replace("-\n", "").replace("\n", "")
+
+                if (yP < footer_threshold):
+                    # RULE: Eliminate single word text which is close to the threshold
+                    if((yP < header_threshold * 2 or yP > footer_threshold_2) and len(text.split(" ")) < 2):
+                        eliminate_log(pN, "Not in threshold but single word", text)
+                    elif(len(text) < 2):
+                        eliminate_log(pN, "Text Length is too small", text)
+                    else:
+                        for line in text_box:
+                            if isinstance(line, LTTextLineHorizontal): # Be sure line is an exact line =) 
+                                size = get_text_size(line)
+                                
+                                # plt_vals.extend([size]*round(len(text)/10))
+                                # x_axis.extend([pN]*round(len(text)/10))
+                                text_boxes.append((xP, yP, xS, yS, size, text))
+                                print((xP, yP, xS, yS, size, text[:50]))
+                                break # analysis of first line is enough
                 else:
-                    for line in text_box:
-                        if isinstance(line, LTTextLineHorizontal): # Be sure line is an exact line =) 
-                            size = get_text_size(line)
-                            text_boxes.append((xP, yP, xS, yS, size, text))
-                            break # analysis of first line is enough
-            else:
-                eliminate_log(pN, "Header / Footer text:", xP, yP, text)
+                    eliminate_log(pN, "Header / Footer text:", xP, yP, text)
     return text_boxes
 
 def generate_content(text_boxes):
@@ -139,21 +153,28 @@ def generate_content(text_boxes):
         #print(text)
             
         # RULE: Remove smallest text - generally image descriptions
-        # Smallest size, I'm sceptical about this.If there is no footer text smallest size will be main text's size
-        smallest_size = min(text_boxes, key=operator.itemgetter(ord_size))[ord_size]
-        if size != smallest_size:
-            # RULE: Merge two lines if second one is beginning with lowercase
-            if text[0] == text[0].lower(): # sentence begins with lower letter
-                # print("found lowercase beginning")
-                # print("content" + content)
-                content = content[:-2] + " " # Remove new line chars (\n) from previous line
-            elif text[-1] not in [".", "!", "?"]:
-                add_new_line = False
-            content += text
-            if add_new_line : content += "\n"
+        # Smallest size, I'm sceptical about this.
+        # If there is no footer text smallest size will be main text's size
+        # If three or more text box has the same smallest size then ignore it.
+        #smallest_size = min(text_boxes, key=operator.itemgetter(ord_size))[ord_size]
+        #if len(list(filter(lambda x: x[ord_size] == smallest_size, text_boxes))) < 3:
+        #    smallest_size = smallest_size
+        #else:
+        #    smallest_size = 0
+        #if size != smallest_size:
+        # else:
+        #     eliminate_log(pN, "Smallest text in the page", text)
+        # RULE: Merge two lines if second one is beginning with lowercase
+        if text[0] == text[0].lower(): # sentence begins with lower letter
+            # print("found lowercase beginning")
+            # print("content" + content)
+            content = content[:-2] + " " # Remove new line chars (\n) from previous line
+        elif text[-1] not in [".", "!", "?"]:
+            add_new_line = False
+        content += text
+        if add_new_line : content += "\n"
             
-        else:
-            eliminate_log(pN, "Smallest text in the page", text)
+
             
     output_text_file.write(content)
 
@@ -165,8 +186,8 @@ def process_article_block(block):
     generate_content(block)
 
 for pN, page in enumerate(PDFPage.get_pages(fp, pagenos, caching=caching, check_extractable=True)):
-    # Cover page ignored.
-    if pN > 0 and pN == 27:
+    # Cover and index pages ignored.
+    if pN > 1 and pN < 31:
         interpreter.process_page(page)
         # LAYOUT INITIALIZATION - Each page may have different width and height
         layout = device.get_result()
@@ -181,7 +202,7 @@ for pN, page in enumerate(PDFPage.get_pages(fp, pagenos, caching=caching, check_
 
         text_boxes = process_layout(layout, pN)
 
-        if (len(text_boxes) > 0):            
+        if len(text_boxes) > 3:            
             text_boxes.sort(key = operator.itemgetter(ord_x_pri, ord_y_pri))
 
             # RULE: remove footer fext like page number and magazine name
@@ -191,8 +212,7 @@ for pN, page in enumerate(PDFPage.get_pages(fp, pagenos, caching=caching, check_
             # Dividing Page in two part with seperator
             seperator_line, space = get_seperator_line( list(map(lambda item: [item[ord_y_pri], item[ord_y_sec]], text_boxes)) )
             
-            print("space", space)
-            print("seperator line: ", seperator_line)
+            print("seperator line: ", seperator_line, "space", space)
 
             if space > 20:
                 above_the_seperator_items = list(filter(lambda text_box: text_box[ord_y_sec] < seperator_line, text_boxes))
@@ -215,5 +235,6 @@ retstr.close()
 log_file.close()
 
 # Plots
-# plt.plot(vals)
-# plt.show()
+# plt.xticks(plt_vals, x_axis)
+# plt.plot(plt_vals)
+plt.show()
--- readme.md
+++
@@ -1,3 +0,0 @@
-# What and Why?
-
-This work is an attempt to get clear text from PDF magazines to provide content for the visually impaired.
