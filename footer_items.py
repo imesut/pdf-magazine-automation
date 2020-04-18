899c621b68cd17c295c2329102e8fef5c6221601
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2020-04-18 16:15:45 +0300

    Changed footer item removal logic with generic logic
    Previous footer removal method was looking for the smallest sizes or looking for the items which is at the bottom.
    Current method maps all of the footer items and determines repeating items by their repeating frequency.

---
+++ footer_items.py
@@ -0,0 +1,27 @@
+import unittest, operator
+
+def clean_single_footer_items(footer_items, unique_items):
+    for item in footer_items:
+        if item in unique_items:
+            unique_items.remove(item)
+            footer_items.remove(item)
+            clean_single_footer_items(footer_items, unique_items)
+
+# Receives an array from tuples like [(yL, yH, 'text'), (yL, yH, 'text') ...]
+def get_repeating_footer_items(footer_items):
+    uniques_a = list(set(footer_items))
+    clean_single_footer_items(footer_items, uniques_a)
+    footer_items = list(set(footer_items))
+    return footer_items
+
+
+# TESTS
+sample_data_from_lezzet = [(686, 727, 'N e v'), (702, 722, 'çorbasını yapın'), (738, 745, 'facebook.com/le'), (738, 745, 'lezzet_dergisi'), (739, 745, 'www.lezzet.com.'), (738, 745, '@Lezzet_Dergisi'), (772, 780, 'NİSAN | MAYIS  '), (739, 758, 'www.dbabone.com'), (701, 731, 'güçlendirsin...'), (719, 741, '  RAMAZAN ÖZEL '), (735, 745, 'SAĞLIKLI SAHUR'), (772, 780, '  LEZZET • NİSA'), (697, 745, 'VEGAN TARİFLERA'), (730, 752, '  ASTROLOJİ'), (690, 701, 'Haydi mutfağa!'), (772, 780, '  LEZZET • NİSA'), (686, 695, '•   çay bardağı'), (696, 705, 'zeytinyağı'), (706, 725, '•   su bardağı '), (726, 735, 'nane'), (736, 745, '•  Tuz'), (772, 780, ' NİSAN | MAYIS '), (690, 699, '•   Hepsini haz'), (700, 729, 'anda limonla ov'), (696, 705, 'soğan'), (706, 725, '•   adet limon '), (726, 735, 'kadar tuz'), (772, 780, '  LEZZET • NİSA'), (772, 780, ' NİSAN | MAYIS '), (697, 724, 'DENİZ ORHUNZIRA'), (728, 747, '@denizorhunklem'), (768, 776, '  LEZZET • NİSA'), (689, 749, 'Eskiden bir sav'), (772, 780, 'NİSAN | MAYIS  '), (768, 776, ' LEZZET • NİSAN'), (772, 780, 'NİSAN | MAYIS  '), (768, 776, ' LEZZET • NİSAN'), (772, 780, '  LEZZET • NİSA'), (685, 694, 'şekeri'), (695, 724, '•   g süt•   ta'), (725, 744, '(Beyazları ve s'), (745, 754, '•   g bitter çi'), (685, 754, ' Yağ ve unu bir'), (772, 780, ' NİSAN | MAYIS '), (726, 735, ' paket toz vani'), (736, 745, '•  - adet orta '), (772, 780, '  LEZZET • NİSA'), (731, 740, ' paket toz vani'), (772, 780, ' NİSAN | MAYIS '), (715, 724, ' paket toz vani'), (725, 744, '•   g çilek (Ay'), (772, 780, '  LEZZET • NİSA'), (684, 693, '(Yıkanmış, ayık'), (694, 703, '•   çay bardağı'), (704, 713, 'fındık '), (724, 753, 'SOS IÇIN•   yem'), (772, 780, ' NİSAN | MAYIS '), (772, 780, '  LEZZET • NİSA'), (772, 780, ' NİSAN  | MAYIS')]
+expected_output_of_lezzet = [(772, 780, ' NİSAN | MAYIS '), (772, 780, '  LEZZET • NİSA'), (768, 776, ' LEZZET • NİSAN'), (772, 780, 'NİSAN | MAYIS  ')]
+
+class TestSeperator(unittest.TestCase):
+    def test_footer_items(self):
+        self.assertListEqual(sorted(get_repeating_footer_items(sample_data_from_lezzet)), sorted(expected_output_of_lezzet))
+
+if __name__ == '__main__':
+    unittest.main()
--- pdf.py
+++ pdf.py
@@ -1,55 +1,77 @@
-
-
-# TODO: remove images with ghostscript
-# TODO stringio may be used
 # TODO add keys to arrays for performance
-# TODO: What can be parameters
+# TODO: What can be functional parameters
 # 1. col_count - 12 worked for Atlas | 10 worked for Ist. Life
+# 2. file name
 
+# PDF MINER
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter, PDFPageAggregator
 from pdfminer.layout import LAParams, LTTextBoxHorizontal, LTChar, LTTextLineHorizontal
 from pdfminer.pdfpage import PDFPage
-from io import StringIO
-from matplotlib import pyplot as plt
+
+# SYSTEM UTILS
 import operator
+import os
+import sys
+# TODO Evaluate time for benchmark purposes
+#import time
+
+# UTILS
+from footer_items import get_repeating_footer_items
 from seperator import get_seperator_line
 
-pdf_file_name = "pozitif"
+# VISUALIZATION
+from matplotlib import pyplot as plt
+
+################################################################################
+
+pdf_file_name = ""
+
+if len(sys.argv) > 1:
+    pdf_file_name = sys.argv[1]
+else:
+    pdf_file_name = "pozitif.pdf"
+
+file_base = pdf_file_name[:-4]
+
+if not os.path.isdir(file_base + "/"):
+    os.mkdir(file_base)
+
+################################################################################
+
 article = ""
 plt_vals = []
-# x_axis = []
 log_file = open(pdf_file_name + ".log", "w")
+pages = []
+width = 0
+height = 0
+first_time = True
 
 # TODO: Change this value for a common approach
-threshold_rate = 0.05
-col_count = 10
-
-resourcemanager = PDFResourceManager()
-retstr = StringIO()
-#codec = 'utf-8'
-laparams = LAParams(detect_vertical=True)
-#device = TextConverter(resourcemanager, retstr, laparams=laparams)
-device = PDFPageAggregator(resourcemanager, laparams=laparams)
-fp = open(pdf_file_name + ".pdf", 'rb')
-interpreter = PDFPageInterpreter(resourcemanager, device)
-caching = True
-pagenos=set()
+threshold_rate = 0  # 0.05
+col_count = 6
 
 output_file_count = 1
 prev_biggest_item_size = 0
+title_size = 0
 ord_x_pri, ord_y_pri, ord_x_sec, ord_y_sec, ord_size, ord_text = 0, 1, 2, 3, 4, 5
+
+################################################################################
+
 
 def eliminate_log(pN, desc, *args):
     log_file.write("p: " + str(pN) + " ELIMINATED: " + desc + str(args) + "\n")
 
+
 def create_file():
     global output_file_count
-    otf = open(pdf_file_name + "_" + str(output_file_count) + ".txt", "w")
+    otf = open(file_base + "/" + file_base + "_" +
+               str(output_file_count) + ".txt", "w")
     output_file_count += 1
     return otf
 
-def get_text_size(horizontal_text_line):
+
+def get_text_size_of_line(horizontal_text_line):
     # RULE: Check if just first char is higher than others
     size = []
     count = 0
@@ -57,51 +79,66 @@ def get_text_size(horizontal_text_line):
         if isinstance(char, LTChar):
             size.append(int(char.size / 5))
             count += 1
-        if count == 5: # Just 5 character is enough
+        if count == 5:  # Max 5 character is enough
             break
     # size returns an array like [7, 1, 1, 1, 1] if just first letter is high
     # We'll return the last item
     return size[-1]
 
+
 output_text_file = create_file()
 
-first_time = False
+
 def calculate_titles_and_insert(text_boxes):
     # Title Calculation
     # When new title calculated seperate files
 
-    global prev_biggest_item_size, output_text_file, first_time
+    global prev_biggest_item_size, output_text_file, first_time, title_size
 
-    #print("text_boxes", text_boxes)
-    biggest_item = max(text_boxes, key=operator.itemgetter(ord_size))
-    #print("biggest_item", biggest_item)
-    biggest_item_size = biggest_item[4]
-    biggest_items = []
-    if prev_biggest_item_size == 0:
-        prev_biggest_item_size = biggest_item_size
-        first_time = True
-    
-    # if biggest_item_size >= prev_biggest_item_size:
-    # There is a threshold
-    print("Title calculation: ", prev_biggest_item_size, biggest_item_size)
-    # Normalize too big titles
-    if biggest_item_size > prev_biggest_item_size:
-        biggest_item_size = prev_biggest_item_size
-    if abs(biggest_item_size - prev_biggest_item_size) <= 2:
-        biggest_items = list(filter(lambda x: x[ord_size] >= biggest_item_size, text_boxes))
-        biggest_items_text = map(lambda x: x[ord_text], biggest_items)
-        # print("biggest_items", biggest_items)
-        title = " ".join(biggest_items_text)
-        title = title.capitalize()
-        #print("title", title)
+    titles = list(filter(lambda x: x[ord_size] >= title_size, text_boxes))
+
+    if len(titles) > 0:
+        merged_title = " ".join(
+            list(map(lambda x: x[ord_text], titles))).capitalize()
         if not first_time:
-            #print("closing file", biggest_item_size, prev_biggest_item_size)
             output_text_file.close()
             output_text_file = create_file()
-        output_text_file.write("# " + title + "\n")
-        prev_biggest_item_size = biggest_item_size
+        output_text_file.write("# " + merged_title + "\n")
         first_time = False
-    return biggest_items
+
+    return titles
+
+    # #print("text_boxes", text_boxes)
+    # biggest_item = max(text_boxes, key=operator.itemgetter(ord_size))
+    # #print("biggest_item", biggest_item)
+    # biggest_item_size = biggest_item[4]
+    # biggest_items = []
+    # if prev_biggest_item_size == 0:
+    #     prev_biggest_item_size = biggest_item_size
+    #     first_time = True
+
+    # if biggest_item_size >= prev_biggest_item_size:
+    # There is a threshold
+    #print("Title calculation: ", prev_biggest_item_size, biggest_item_size)
+    # Normalize too big titles
+    # if biggest_item_size > prev_biggest_item_size:
+    #     biggest_item_size = prev_biggest_item_size
+    # if abs(biggest_item_size - prev_biggest_item_size) <= 2:
+    #     biggest_items = list(filter(lambda x: x[ord_size] >= biggest_item_size, text_boxes))
+    #     biggest_items_text = map(lambda x: x[ord_text], biggest_items)
+    #     # print("biggest_items", biggest_items)
+    #     title = " ".join(biggest_items_text)
+    #     title = title.capitalize()
+    #     #print("title", title)
+    #     if not first_time:
+    #         #print("closing file", biggest_item_size, prev_biggest_item_size)
+    #         output_text_file.close()
+    #         output_text_file = create_file()
+    #     output_text_file.write("# " + title + "\n")
+    #     prev_biggest_item_size = biggest_item_size
+    #     first_time = False
+    # return biggest_items
+
 
 def process_layout(layout, pN):
     text_boxes = []
@@ -112,9 +149,9 @@ def process_layout(layout, pN):
             # (Sometimes two successing texts x value differ a little. eliminating differences by assigning col values)
             xP = round(text_box.x0 / col_width_divider)
             yP = height-round(text_box.y1)
-            xS = round(text_box.x1 / col_width_divider) 
+            xS = round(text_box.x1 / col_width_divider)
             yS = height-round(text_box.y0)
-                
+
             # Plain
             text = text_box.get_text()
 
@@ -126,22 +163,22 @@ def process_layout(layout, pN):
                 if (yP < footer_threshold):
                     # RULE: Eliminate single word text which is close to the threshold
                     if((yP < header_threshold * 2 or yP > footer_threshold_2) and len(text.split(" ")) < 2):
-                        eliminate_log(pN, "Not in threshold but single word", text)
+                        eliminate_log(
+                            pN, "Not in threshold but single word", text)
                     elif(len(text) < 2):
                         eliminate_log(pN, "Text Length is too small", text)
                     else:
                         for line in text_box:
-                            if isinstance(line, LTTextLineHorizontal): # Be sure line is an exact line =) 
-                                size = get_text_size(line)
-                                
-                                # plt_vals.extend([size]*round(len(text)/10))
-                                # x_axis.extend([pN]*round(len(text)/10))
+                            # Be sure line is an exact line =)
+                            if isinstance(line, LTTextLineHorizontal):
+                                size = get_text_size_of_line(line)
                                 text_boxes.append((xP, yP, xS, yS, size, text))
-                                print((xP, yP, xS, yS, size, text[:50]))
-                                break # analysis of first line is enough
+                                #print((xP, yP, xS, yS, size, text[:50]))
+                                break  # analysis of first line is enough
                 else:
                     eliminate_log(pN, "Header / Footer text:", xP, yP, text)
     return text_boxes
+
 
 def generate_content(text_boxes):
     content = ""
@@ -150,91 +187,160 @@ def generate_content(text_boxes):
         size = i[ord_size]
         text = i[ord_text]
         add_new_line = True
-        #print(text)
-            
+        # print(text)
+
         # RULE: Remove smallest text - generally image descriptions
         # Smallest size, I'm sceptical about this.
         # If there is no footer text smallest size will be main text's size
         # If three or more text box has the same smallest size then ignore it.
         #smallest_size = min(text_boxes, key=operator.itemgetter(ord_size))[ord_size]
-        #if len(list(filter(lambda x: x[ord_size] == smallest_size, text_boxes))) < 3:
+        # if len(list(filter(lambda x: x[ord_size] == smallest_size, text_boxes))) < 3:
         #    smallest_size = smallest_size
-        #else:
+        # else:
         #    smallest_size = 0
-        #if size != smallest_size:
+        # if size != smallest_size:
         # else:
         #     eliminate_log(pN, "Smallest text in the page", text)
         # RULE: Merge two lines if second one is beginning with lowercase
-        if text[0] == text[0].lower(): # sentence begins with lower letter
+        if text[0] == text[0].lower():  # sentence begins with lower letter
             # print("found lowercase beginning")
             # print("content" + content)
-            content = content[:-2] + " " # Remove new line chars (\n) from previous line
+            # Remove new line chars (\n) from previous line
+            content = content[:-2] + " "
         elif text[-1] not in [".", "!", "?"]:
             add_new_line = False
-        content += text
-        if add_new_line : content += "\n"
-            
+        # Add space after punctuation mark, begin new sentence
+        content += text + " "
+        if add_new_line:
+            content += "\n"
 
-            
     output_text_file.write(content)
 
+
 def process_article_block(block):
-    block.sort(key = operator.itemgetter(ord_x_pri, ord_y_pri))
+    block.sort(key=operator.itemgetter(ord_x_pri, ord_y_pri))
     titles = calculate_titles_and_insert(block)
     for item in titles:
         block.remove(item)
     generate_content(block)
 
+
+def clean_chars_for_bottom_item(text):
+    return "".join([char for char in text if not char.isdigit()])
+
+################################################################################
+
+resourcemanager = PDFResourceManager()
+laparams = LAParams(detect_vertical=True, all_texts=True)
+device = PDFPageAggregator(resourcemanager, laparams=laparams)
+fp = open(pdf_file_name, 'rb')
+interpreter = PDFPageInterpreter(resourcemanager, device)
+caching = True
+pagenos = set()
+print("started to parse: ", pdf_file_name)
+
 for pN, page in enumerate(PDFPage.get_pages(fp, pagenos, caching=caching, check_extractable=True)):
     # Cover and index pages ignored.
-    if pN > 1 and pN < 31:
+    if pN > 1:
         interpreter.process_page(page)
-        # LAYOUT INITIALIZATION - Each page may have different width and height
         layout = device.get_result()
-        width, height = round( page.mediabox[2] ), round( page.mediabox[3] )
+        header_threshold = 0  # round(height * threshold_rate)
+        width = round(page.mediabox[2])
+        height = round(page.mediabox[3])
         col_width_divider = width / col_count
-        half_line = height / 2
-        header_threshold = 0 #round(height * threshold_rate)
         footer_threshold = round(height * (1 - threshold_rate))
         footer_threshold_2 = round(height * (1 - threshold_rate * 2))
-        
-        print(pN, "PAGE INFO: ", "width", width, "height", height, "header_threshold", header_threshold, "footer_threshold", footer_threshold)
-
+        print(pN, "FETCHING PAGE: ", "width", width, "height", height)
         text_boxes = process_layout(layout, pN)
-
-        if len(text_boxes) > 3:            
-            text_boxes.sort(key = operator.itemgetter(ord_x_pri, ord_y_pri))
-
-            # RULE: remove footer fext like page number and magazine name
-            bottom_item = max(text_boxes, key=operator.itemgetter(ord_x_pri))
-            text_boxes.remove(bottom_item)
-
-            # Dividing Page in two part with seperator
-            seperator_line, space = get_seperator_line( list(map(lambda item: [item[ord_y_pri], item[ord_y_sec]], text_boxes)) )
-            
-            print("seperator line: ", seperator_line, "space", space)
-
-            if space > 20:
-                above_the_seperator_items = list(filter(lambda text_box: text_box[ord_y_sec] < seperator_line, text_boxes))
-                for item in above_the_seperator_items: text_boxes.remove(item)
-                below_the_seperator_items = text_boxes
-                del text_boxes
-
-                #print(above_the_seperator_items)
-                #print(below_the_seperator_items)
-                
-                process_article_block(above_the_seperator_items)
-                process_article_block(below_the_seperator_items)
-
-            else:
-                process_article_block(text_boxes)
+        pages.append(text_boxes)
 
 fp.close()
 device.close()
-retstr.close()
+
+################################################################################
+
+print("started to process: ", pdf_file_name)
+
+
+bottom_items = []
+sizes = []
+
+top = height * 0.1
+bottom = height - 100 # height * 0.85
+
+
+# LAYOUT ANALYSIS
+for page in pages:
+    for tb in page:
+        size = tb[ord_size]
+        sizes.append(size)
+        if tb[ord_y_pri] > bottom:
+            # Use shortened text
+            text = clean_chars_for_bottom_item(tb[ord_text])
+            text = text[:15]
+            bottom_items.append((tb[ord_y_pri], tb[ord_y_sec], text))
+
+sizes.sort()
+title_size = sizes[int(len(sizes) * 0.85)]
+# print(sizes)
+print("title size: ", title_size)
+del sizes
+
+################################################################################
+
+# RULE: Remove footer fext like page number and magazine name
+# Get footer items and remove from all of the pages
+footer_items_to_remove = get_repeating_footer_items(bottom_items)
+print("footer_items_to_remove", footer_items_to_remove)
+for page in pages:
+    for text_box in page:
+        if text_box[ord_y_pri] > bottom:
+            for waste in footer_items_to_remove:
+                yl_i = text_box[ord_y_pri]
+                yh_i = text_box[ord_y_sec]
+                text_i = clean_chars_for_bottom_item(text_box[ord_text][:25])
+                yl_w = waste[0]
+                yh_w = waste[1]
+                text_w = waste[2]
+                if yl_i == yl_w and yh_i == yh_w and text_i.startswith(text_w):
+                    page.remove(text_box)
+                    print("removed", text_box)
+
+################################################################################
+
+for page in pages:
+    text_boxes = page
+    # if there is too many smal boxes that maybe tabular data
+    if len(text_boxes) > 3 and len(text_boxes) < 20:
+        text_boxes.sort(key=operator.itemgetter(ord_x_pri, ord_y_pri))
+
+        # TODO this process is slow if items is too much
+        # TODO maintenance this after
+        # Dividing Page in two part with seperator
+        #seperator_line, space = get_seperator_line( list(map(lambda item: [item[ord_y_pri], item[ord_y_sec]], text_boxes)) )
+
+        #print("seperator line: ", seperator_line, "space", space)
+
+        space = 0
+        if space > 20:
+            above_the_seperator_items = list(
+                filter(lambda text_box: text_box[ord_y_sec] < seperator_line, text_boxes))
+            for item in above_the_seperator_items:
+                text_boxes.remove(item)
+            below_the_seperator_items = text_boxes
+            del text_boxes
+
+            # print(above_the_seperator_items)
+            # print(below_the_seperator_items)
+
+            process_article_block(above_the_seperator_items)
+            process_article_block(below_the_seperator_items)
+
+        else:
+            process_article_block(text_boxes)
+
 log_file.close()
 
-# Plots
-# plt.xticks(plt_vals, x_axis)
-# plt.plot(plt_vals)
-plt.show()
+# # Plots
+# # plt.plot(sizes)
+# # plt.show()
